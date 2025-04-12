e2a7047d9a32f7c4275d9a83d916cbfcbcfac7da
Bendukuri Supriya <Suppu@1996>
2025-04-12 21:44:28 +0530

    Resume Upload project

---
+++ src/test/java/UploadResume/ResumeUploading.java
@@ -0,0 +1,29 @@
+package UploadResume;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ResumeUploading {
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void launchUrl() {
+		
+		driver = new ChromeDriver();
+		driver.get("https://www.naukri.com/nlogin/login");
+		
+		
+	}
+	
+	@Test
+	public void login() throws InterruptedException {
+		driver.findElement(By.id("usernameField")).sendKeys("supriya.bmm43@gmail.com");
+		driver.findElement(By.id("passwordField")).sendKeys("Suppu1234");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[text()='Login']")).click();
+	}
+	
+}
